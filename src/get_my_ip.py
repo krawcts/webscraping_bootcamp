7346c9ac7fcc51bde07bbbbccd4999442381be54
krawcts <lucassampaio22k@gmail.com>
2024-06-21 16:39:35 -0300

    get ip code

---
+++ src/get_my_ip.py
@@ -0,0 +1,12 @@
+import requests 
+
+url = "http://lumtest.com/myip.json"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    
+    print(response.json())
+
+else:
+    print("Erro")
